7c923b54276cdcbec74f9d4fe8d554bcd3fcde67
Jere Julian <jere@arista.com>
2015-09-04 15:54:30 +0000

    Change mlag_portchannel from a class to a defined type for better integration with hiera

--- manifests/mlag_portchannel.pp
+++ manifests/mlag_portchannel.pp
@@ -1,30 +1,44 @@
-class eos_config::mlag_portchannel (
-  $mlag,
-  $port_channel,
+define eos_config::mlag_portchannel (
+  $mlag = undef,
+  $port_channel = undef,
   $members,
+  $trunk_native_vlan = undef,
+  $trunk_allowed_vlans = undef,
 ) {
+
+  if $mlag {
+    $mlagid = $mlag
+  } else {
+    $mlagid = $name
+  }
+
+  if $port_channel {
+    $port_channelid = $port_channel
+  } else {
+    $port_channelid = "Port-Channel${mlagid}"
+  }
 
   eos_config::portchannel::member { $members: }
 
-  eos_interface { $port_channel:
+  eos_interface { $port_channelid:
     description => 'managed by puppet',
   }
 
-  eos_switchport { $port_channel:
+  eos_switchport { $port_channelid:
     mode => trunk,
-    trunk_native_vlan   => 100,
-    trunk_allowed_vlans => ['101', '102', '103', '104'],
+    trunk_native_vlan   => $trunk_native_vlan,
+    trunk_allowed_vlans => $trunk_allowed_vlans,
   }
 
-  eos_portchannel { $port_channel:
+  eos_portchannel { $port_channelid:
     ensure    => present,
     lacp_mode => active,
     members   => $members,
-    require   => Eos_interface[$port_channel],
+    require   => Eos_interface[$port_channelid],
   }
 
-  eos_mlag_interface { $port_channel:
-    mlag_id => $mlag,
-    require => Eos_portchannel[$port_channel],
+  eos_mlag_interface { $port_channelid:
+    mlag_id => $mlagid,
+    require => Eos_portchannel[$port_channelid],
   }
 }
