4630bebe0d7b6c3cf9fd8df770594bda6048b8dc
Jere Julian <jere@arista.com>
2015-10-01 22:47:31 -0400

    Resolve lint and syntax issues

--- manifests/vlans.pp
+++ manifests/vlans.pp
@@ -2,7 +2,7 @@ class eos_config::vlans ($purge = false) {
   $vlans = hiera('vlans', {})
   create_resources( eos_vlan, $vlans )
 
-  resources { "eos_vlan":
+  resources { 'eos_vlan':
     purge => $purge,
   }
 }
