3b0b2cf8a6b3b903863f8fb6b5254ebb5c014fc0
Jere Julian <jere@arista.com>
2015-10-05 16:14:14 -0400

    add dummy code for test

--- manifests/vlans.pp
+++ manifests/vlans.pp
@@ -3,6 +3,8 @@ class eos_config::vlans ($purge = false) {
   $vlans = hiera('vlans', {})
   create_resources( eos_vlan, $vlans )
 
+  $test = generate('/bin/grep', 'localhost', '/etc/hosts')
+
   resources { 'eos_vlan':
     purge => $purge,
   }
