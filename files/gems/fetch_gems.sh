5a490d7cbb8c8f0161d668b11732df9a43e4205b
Jere Julian <jere@arista.com>
2015-10-14 15:48:05 -0400

    Add script to fetch the desired rubygems so switches don't need outside access

---
+++ files/gems/fetch_gems.sh
@@ -0,0 +1,6 @@
+export PATH="/opt/puppetlabs/puppet/bin/gem:${PATH}"
+
+gem  fetch inifile --version '= 3.0.0'
+gem  fetch netaddr --version '= 1.5.0'
+gem  fetch net_http_unix --version '= 0.2.1'
+gem  fetch rbeapi --version '= 0.3.0'
