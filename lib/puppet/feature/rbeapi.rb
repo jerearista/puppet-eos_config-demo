3b66cb2fb56bbf9534d1bde3fe2922f9a6556bfd
Jere Julian <jere@arista.com>
2015-10-14 15:33:40 -0400

    Remove commented code

--- lib/puppet/feature/rbeapi.rb
+++ lib/puppet/feature/rbeapi.rb
@@ -1,9 +1,3 @@
 require 'puppet/util/feature'
-#require 'puppet/util/libuser'
 
 Puppet.features.add(:rbeapi, :libs => ["rbeapi"])
-#Puppet.features.add(:rbeapi) {
-#   File.executable?("/usr/sbin/lgroupadd") and
-#   File.executable?("/usr/sbin/luseradd")  and
-#   Puppet::FileSystem.exist?(Puppet::Util::Libuser.getconf)
-#}
