63f81e33ed23b97ab01e16259222a7ea82bf1d65
Jere Julian <jere@arista.com>
2016-08-12 10:25:08 -0400

    Remove dummy code

--- manifests/vlans.pp
+++ manifests/vlans.pp
@@ -4,8 +4,6 @@ class eos_config::vlans ($purge = false) {
   $vlans = hiera('vlans', {})
   create_resources( eos_vlan, $vlans )
 
-  $test = generate('/bin/grep', 'localhost', '/etc/hosts')
-
   resources { 'eos_vlan':
     purge => $purge,
   }
